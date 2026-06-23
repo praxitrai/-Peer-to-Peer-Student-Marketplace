9beb1d13eb5fb833e91a97ae71d6768cc3181b01
Praxit Rai <praxit14@gmail.com>
2026-06-23 09:52:28 +0545

    Set up Flask app with debug mode
    Initialize Flask app with debug mode for development.

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
+from app import create_app
 
+app = create_app()
+
+if __name__ == '__main__':
+    # Debug mode activated for instant server reloads during your development cycle
+    app.run(debug=True, port=5000)
